9476a0620800e66a7c49072f908bdd9b6d6de350
FacuRCh <facuromeroch@gmail.com>
2023-08-30 15:06:22 -0300

    Formulario login

---
+++ CapaPresentacion/Login.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace CapaPresentacion
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btningresar = new FontAwesome.Sharp.IconButton();
+            this.btncancelar = new FontAwesome.Sharp.IconButton();
+            this.lnrodocumento = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtingresar = new System.Windows.Forms.TextBox();
+            this.txtcontraseña = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 226);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btningresar
+            // 
+            this.btningresar.BackColor = System.Drawing.Color.Green;
+            this.btningresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btningresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btningresar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btningresar.Icon = FontAwesome.Sharp.IconChar.Key;
+            this.btningresar.IconColor = System.Drawing.Color.White;
+            this.btningresar.IconSize = 21;
+            this.btningresar.Image = ((System.Drawing.Image)(resources.GetObject("btningresar.Image")));
+            this.btningresar.Location = new System.Drawing.Point(245, 164);
+            this.btningresar.Name = "btningresar";
+            this.btningresar.Size = new System.Drawing.Size(131, 28);
+            this.btningresar.TabIndex = 1;
+            this.btningresar.Text = "Ingresar";
+            this.btningresar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btningresar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btningresar.UseVisualStyleBackColor = false;
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.BackColor = System.Drawing.Color.Red;
+            this.btncancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncancelar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btncancelar.Icon = FontAwesome.Sharp.IconChar.Times;
+            this.btncancelar.IconColor = System.Drawing.Color.White;
+            this.btncancelar.IconSize = 21;
+            this.btncancelar.Image = ((System.Drawing.Image)(resources.GetObject("btncancelar.Image")));
+            this.btncancelar.Location = new System.Drawing.Point(408, 164);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(131, 28);
+            this.btncancelar.TabIndex = 2;
+            this.btncancelar.Text = "Cancelar";
+            this.btncancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btncancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btncancelar.UseVisualStyleBackColor = false;
+            // 
+            // lnrodocumento
+            // 
+            this.lnrodocumento.AutoSize = true;
+            this.lnrodocumento.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnrodocumento.Location = new System.Drawing.Point(279, 27);
+            this.lnrodocumento.Name = "lnrodocumento";
+            this.lnrodocumento.Size = new System.Drawing.Size(113, 13);
+            this.lnrodocumento.TabIndex = 3;
+            this.lnrodocumento.Text = "Nro de Documento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(282, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Contraseña";
+            // 
+            // txtingresar
+            // 
+            this.txtingresar.Location = new System.Drawing.Point(276, 43);
+            this.txtingresar.Name = "txtingresar";
+            this.txtingresar.Size = new System.Drawing.Size(238, 20);
+            this.txtingresar.TabIndex = 5;
+            // 
+            // txtcontraseña
+            // 
+            this.txtcontraseña.Location = new System.Drawing.Point(276, 111);
+            this.txtcontraseña.Name = "txtcontraseña";
+            this.txtcontraseña.Size = new System.Drawing.Size(238, 20);
+            this.txtcontraseña.TabIndex = 6;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(551, 226);
+            this.Controls.Add(this.txtcontraseña);
+            this.Controls.Add(this.txtingresar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lnrodocumento);
+            this.Controls.Add(this.btncancelar);
+            this.Controls.Add(this.btningresar);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Login";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private FontAwesome.Sharp.IconButton btningresar;
+        private FontAwesome.Sharp.IconButton btncancelar;
+        private System.Windows.Forms.Label lnrodocumento;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtingresar;
+        private System.Windows.Forms.TextBox txtcontraseña;
+    }
+}
