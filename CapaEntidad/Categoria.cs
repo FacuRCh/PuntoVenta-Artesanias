87c54f548980756775a5e8bd2121f90cd612056c
FacuRCh <facuromeroch@gmail.com>
2023-08-30 19:59:58 -0300

    Entidades faltantes agregadas

--- CapaEntidad/Categoria.cs
+++ CapaEntidad/Categoria.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace CapaEntidad
 {
-    public class Permiso
+    public class Categoria
     {
-        public int IdPermiso { get; set; } 
-        public Rol IdRol { get; set; }
-        public string NombreMenu { get; set; }
+        public int IdCategoria { get; set; }
+        public string Descripcion { get; set; }
+        public bool Estado { get; set; }
         public string FechaRegistro { get; set; }
     }
 }
--- CapaEntidad/Proveedor.cs
+++ CapaEntidad/Proveedor.cs
@@ -6,14 +6,13 @@ using System.Threading.Tasks;
 
 namespace CapaEntidad
 {
-    public class Usuario
+    public class Proveedor
     {
-        public int IdUsuario { get; set; }
+        public int idProveedor { get; set; }
         public string Documento { get; set; }
-        public string NombreCompleto { get; set; }
+        public string RazonSocial { get; set; }
         public string Correo { get; set; }
-        public string Clave { get; set; }
-        public Rol IdRol { get; set; }
+        public string Telefono { get; set; }
         public bool Estado { get; set; }
         public string FechaRegistro { get; set; }
     }
