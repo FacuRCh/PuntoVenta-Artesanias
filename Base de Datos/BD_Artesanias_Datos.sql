9ce6c9bbbc8bc4fae8f71bddffbab9734ef86670
FacuRCh <facuromeroch@gmail.com>
2023-08-31 17:49:50 -0300

    Update BD_Artesanias_Datos.sql

--- Base de Datos/BD_Artesanias_Datos.sql
+++ Base de Datos/BD_Artesanias_Datos.sql
@@ -8,14 +8,21 @@ insert into Rol(Descripcion) values ('ADMIN')
 insert into Rol(Descripcion) values ('EMPLEADO')
 
 insert into Usuario(Documento,Nombre,Apellido,Correo,Clave,IdRol,Estado) values ('21100100','Enzo','Medina','enzo912@gmail.com','enzo24',1,1)
+insert into Usuario(Documento,Nombre,Apellido,Correo,Clave,IdRol,Estado) values ('22100100','Julian','Romero','julian1212@gmail.com','julian12',2,1)
+insert into Usuario(Documento,Nombre,Apellido,Correo,Clave,IdRol,Estado) values ('23100100','Roman','Yiyo','roman10@gmail.com','roman10',3,1)
+
 
 delete from Usuario
 
-DBCC CHECKIDENT(Usuario, RESEED,0)
+DBCC CHECKIDENT(Permiso, RESEED,0)
 
 Select * from Permiso
 
+delete from Permiso
+
 insert into Permiso(IdRol,NombreMenu) values
+(1,'menuusuario'),
+(1,'menureportes'),
 (1,'menuacercade')
 
 insert into Permiso(IdRol,NombreMenu) values
@@ -29,3 +36,8 @@ insert into Permiso(IdRol,NombreMenu) values
 (3,'menucompras'),
 (3,'menuclientes'),
 (3,'menuacercade')
+
+select p.IdRol, p.NombreMenu from Permiso p
+inner join Rol r on r.IdRol = p.IdRol
+inner join Usuario u on u.IdRol = r.IdRol
+where u.IdUsuario = 1
