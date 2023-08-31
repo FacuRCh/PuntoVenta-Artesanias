da231acded5268d44534d68e6dc711c2d9595121
FacuRCh <facuromeroch@gmail.com>
2023-08-31 08:49:38 -0300

    Separacion nombre apellido Usuario

--- CapaEntidad/Usuario.cs
+++ CapaEntidad/Usuario.cs
@@ -10,7 +10,8 @@ namespace CapaEntidad
     {
         public int IdUsuario { get; set; }
         public string Documento { get; set; }
-        public string NombreCompleto { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
         public string Correo { get; set; }
         public string Clave { get; set; }
         public Rol IdRol { get; set; }
