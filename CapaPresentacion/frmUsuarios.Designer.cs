0da043629475c49d918b64fce013252435260279
IanTaboada76 <115680440+IanTaboada76@users.noreply.github.com>
2023-08-31 18:32:29 -0300

    Pequeño formulario de usuario

--- CapaPresentacion/frmUsuarios.Designer.cs
+++ CapaPresentacion/frmUsuarios.Designer.cs
@@ -29,24 +29,260 @@ namespace CapaPresentacion
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmUsuarios));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBoxRol = new System.Windows.Forms.ComboBox();
+            this.comboBoxEstado = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.botonguardarusuario = new FontAwesome.Sharp.IconButton();
+            this.botoneliminarusuario = new FontAwesome.Sharp.IconButton();
+            this.botoneditarusuario = new FontAwesome.Sharp.IconButton();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(113, 147);
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.Size = new System.Drawing.Size(354, 621);
             this.label1.TabIndex = 0;
-            this.label1.Text = "hay habra zaraza con Z";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(93, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nro Documento:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(93, 372);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Rol:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(92, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Confirmar Contraseña:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(92, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Contraseña:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(93, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Correo:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(93, 80);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Nombre:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(93, 419);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Estado:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(93, 134);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 17);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Apellido:";
+            // 
+            // comboBoxRol
+            // 
+            this.comboBoxRol.FormattingEnabled = true;
+            this.comboBoxRol.Location = new System.Drawing.Point(95, 392);
+            this.comboBoxRol.Name = "comboBoxRol";
+            this.comboBoxRol.Size = new System.Drawing.Size(135, 24);
+            this.comboBoxRol.TabIndex = 9;
+            // 
+            // comboBoxEstado
+            // 
+            this.comboBoxEstado.FormattingEnabled = true;
+            this.comboBoxEstado.Location = new System.Drawing.Point(96, 439);
+            this.comboBoxEstado.Name = "comboBoxEstado";
+            this.comboBoxEstado.Size = new System.Drawing.Size(135, 24);
+            this.comboBoxEstado.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 22);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 347);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 22);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(96, 284);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(134, 22);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(95, 219);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(134, 22);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(95, 163);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(135, 22);
+            this.textBox5.TabIndex = 15;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(95, 109);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(135, 22);
+            this.textBox6.TabIndex = 16;
+            // 
+            // botonguardarusuario
+            // 
+            this.botonguardarusuario.BackColor = System.Drawing.Color.Green;
+            this.botonguardarusuario.ForeColor = System.Drawing.Color.White;
+            this.botonguardarusuario.Icon = FontAwesome.Sharp.IconChar.FloppyO;
+            this.botonguardarusuario.IconColor = System.Drawing.Color.White;
+            this.botonguardarusuario.IconSize = 16;
+            this.botonguardarusuario.Image = ((System.Drawing.Image)(resources.GetObject("botonguardarusuario.Image")));
+            this.botonguardarusuario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.botonguardarusuario.Location = new System.Drawing.Point(103, 483);
+            this.botonguardarusuario.Name = "botonguardarusuario";
+            this.botonguardarusuario.Size = new System.Drawing.Size(113, 39);
+            this.botonguardarusuario.TabIndex = 17;
+            this.botonguardarusuario.Text = "Guardar";
+            this.botonguardarusuario.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.botonguardarusuario.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.botonguardarusuario.UseVisualStyleBackColor = false;
+            // 
+            // botoneliminarusuario
+            // 
+            this.botoneliminarusuario.BackColor = System.Drawing.Color.Firebrick;
+            this.botoneliminarusuario.ForeColor = System.Drawing.Color.White;
+            this.botoneliminarusuario.Icon = FontAwesome.Sharp.IconChar.TrashO;
+            this.botoneliminarusuario.IconColor = System.Drawing.Color.White;
+            this.botoneliminarusuario.IconSize = 16;
+            this.botoneliminarusuario.Image = ((System.Drawing.Image)(resources.GetObject("botoneliminarusuario.Image")));
+            this.botoneliminarusuario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.botoneliminarusuario.Location = new System.Drawing.Point(103, 571);
+            this.botoneliminarusuario.Name = "botoneliminarusuario";
+            this.botoneliminarusuario.Size = new System.Drawing.Size(113, 38);
+            this.botoneliminarusuario.TabIndex = 18;
+            this.botoneliminarusuario.Text = "Eliminar";
+            this.botoneliminarusuario.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.botoneliminarusuario.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.botoneliminarusuario.UseVisualStyleBackColor = false;
+            // 
+            // botoneditarusuario
+            // 
+            this.botoneditarusuario.BackColor = System.Drawing.Color.RoyalBlue;
+            this.botoneditarusuario.ForeColor = System.Drawing.Color.White;
+            this.botoneditarusuario.Icon = FontAwesome.Sharp.IconChar.Pencil;
+            this.botoneditarusuario.IconColor = System.Drawing.Color.White;
+            this.botoneditarusuario.IconSize = 16;
+            this.botoneditarusuario.Image = ((System.Drawing.Image)(resources.GetObject("botoneditarusuario.Image")));
+            this.botoneditarusuario.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.botoneditarusuario.Location = new System.Drawing.Point(103, 528);
+            this.botoneditarusuario.Name = "botoneditarusuario";
+            this.botoneditarusuario.Size = new System.Drawing.Size(113, 37);
+            this.botoneditarusuario.TabIndex = 19;
+            this.botoneditarusuario.Text = "Editar";
+            this.botoneditarusuario.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.botoneditarusuario.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.botoneditarusuario.UseVisualStyleBackColor = false;
             // 
             // frmUsuarios
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(861, 621);
+            this.Controls.Add(this.botoneditarusuario);
+            this.Controls.Add(this.botoneliminarusuario);
+            this.Controls.Add(this.botonguardarusuario);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBoxEstado);
+            this.Controls.Add(this.comboBoxRol);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmUsuarios";
             this.Text = "frmUsuarios";
             this.ResumeLayout(false);
@@ -57,5 +293,24 @@ namespace CapaPresentacion
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBoxRol;
+        private System.Windows.Forms.ComboBox comboBoxEstado;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private FontAwesome.Sharp.IconButton botonguardarusuario;
+        private FontAwesome.Sharp.IconButton botoneliminarusuario;
+        private FontAwesome.Sharp.IconButton botoneditarusuario;
     }
 }
