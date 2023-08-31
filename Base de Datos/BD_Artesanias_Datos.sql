4aedb9d78eb9433d1329257bfe9398abb9c879ac
IanTaboada76 <115680440+IanTaboada76@users.noreply.github.com>
2023-08-31 16:27:50 -0300

    Update BD_Artesanias_Datos.sql

--- Base de Datos/BD_Artesanias_Datos.sql
+++ Base de Datos/BD_Artesanias_Datos.sql
@@ -4,6 +4,8 @@ select * from Usuario
 select * from Rol
 
 insert into Rol(Descripcion) values ('SUPER ADMIN')
+insert into Rol(Descripcion) values ('ADMIN')
+insert into Rol(Descripcion) values ('EMPLEADO')
 
 insert into Usuario(Documento,Nombre,Apellido,Correo,Clave,IdRol,Estado) values ('21100100','Enzo','Medina','enzo912@gmail.com','enzo24',1,1)
 
@@ -11,3 +13,19 @@ delete from Usuario
 
 DBCC CHECKIDENT(Usuario, RESEED,0)
 
+Select * from Permiso
+
+insert into Permiso(IdRol,NombreMenu) values
+(1,'menuacercade')
+
+insert into Permiso(IdRol,NombreMenu) values
+(2,'menuusuario'),
+(2,'menumantenedor'),
+(2,'menuproveedores'),
+(2,'menuacercade')
+
+insert into Permiso(IdRol,NombreMenu) values
+(3,'menuventas'),
+(3,'menucompras'),
+(3,'menuclientes'),
+(3,'menuacercade')
